cc7d3b85f860d1de7babb7a9de566c87a7231fa6
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-04-30 15:46:00 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == '.'
   end
 
   def question?
-
+    self[-1] == '?'
   end
 
   def exclamation?
-
+    self[-1] == '!'
   end
 
   def count_sentences
-
+    # count = 0
+    # count = count + self.count('.')
+    # count = count + self.count('?')
+    # count = count + self.count('!')
+    # array = self.split('.').reject { |c| c.empty? }join('.').split('?').reject { |c| c.empty? }.join('?').split('!').reject { |c| c.empty? }.join('!')
+    #  .join.split('?').join.split('!')
+    # .reject { |c| c.empty? }
+    array = self.squeeze.split('')
+    puts array
+    count = 0
+    array.each do |char|
+      if (char == '?' || char == '!' || char == '.')
+        count += 1
+      end
+    end
+    count
   end
 end
